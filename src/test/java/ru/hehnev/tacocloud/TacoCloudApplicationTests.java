b8609f0920b5a6bbc9165315d315358cbb3d642c
maniana <manianahek@gmail.com>
2022-11-04 21:19:31 +0300

    Initial commit

---
+++ src/test/java/ru/hehnev/tacocloud/TacoCloudApplicationTests.java
@@ -0,0 +1,33 @@
+package ru.hehnev.tacocloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import ru.hehnev.tacocloud.controllers.HomeController;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+@WebMvcTest(HomeController.class)
+class TacoCloudApplicationTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+
+
+    @Test
+    void testHomePage() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("home"))
+                .andExpect(content().string(
+                        containsString("Welcome to SHAWERMAS!!!")
+                ));
+    }
+
+}
