f8d39ea66abf049be711571ca0e4c80757a9db2b
hehne <manianahek@gmail.com>
2023-06-05 20:19:01 +0300

    version 2

---
+++ src/main/java/ru/hehnev/tacocloud/model/Taco.java
@@ -0,0 +1,19 @@
+package ru.hehnev.tacocloud.model;
+
+import lombok.Data;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.Collection;
+
+@Data
+public class Taco {
+
+    @NotNull
+    @Size(min = 5, message = "Name must be at least 5 characters long")
+    private String name;
+
+    @NotNull
+    @Size(min = 1, message = "You must choose at least 1 ingredient")
+    private Collection<Ingredient> ingredients;
+}
